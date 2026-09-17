9a202c94e1da5779280ba794aeeda6cbeee75645
hanomag123 <zhenyaglibko@gmail.com>
2026-09-17 11:23:09 +0300

    install filepond

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -464,4 +464,10 @@ document.addEventListener("DOMContentLoaded", () => {
     );
   }
   addMask();
+
+  // Get a reference to the file input element
+    const inputElement = document.querySelector('input[type="file"]');
+
+    // Create a FilePond instance
+    const pond = FilePond.create(inputElement);
 });
