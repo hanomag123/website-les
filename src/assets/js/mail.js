9107c5926d938222dc9f44ec9ec3bdfeaeaf25ca
XitPC <zhenyaglibko@gmail.com>
2026-09-26 15:14:00 +0300

    complete some modals

---
+++ src/assets/js/mail.js
@@ -0,0 +1,36 @@
+const forms = document.querySelectorAll(".modal form, .spec-form form, .contact-form form");
+if (forms) {
+  forms.forEach(form => {
+    form.addEventListener("validated-submit", function () {
+      event.preventDefault();
+      const formData = new FormData(this);
+      const parent = this.closest('.modal');
+      const feedback = document.querySelector('#feedback');
+      for(const key of formData.entries()) {
+        console.log(key);
+      }
+  
+      this.reset();
+      const haveText = document.querySelectorAll('.havetext')
+      if (haveText.length) {
+        haveText.forEach(el => {
+          el.classList.remove('havetext')
+        })
+      }
+  
+      if (parent && parent.classList.contains('modal-with-feedback')) {
+        parent.classList.add('--sended');
+        return;
+      }
+  
+      if (parent && 'closeModal' in parent) {
+        parent.closeModal()
+      } 
+
+      if (feedback && 'openModal' in feedback) {
+        feedback.openModal()
+      }
+    });
+  })
+
+}
